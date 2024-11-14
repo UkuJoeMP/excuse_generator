ed171e84d3dc4793b9fca128fc87dbe54644a48d
UkuJoeMP <josemipinton@gmail.com>
2024-11-14 02:03:58 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = document.getElementById("excuse");
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let randomWho = Math.floor(Math.random() * who.length);
+  console.log(randomWho);
+
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let randomAction = Math.floor(Math.random() * action.length);
+
+  let what = ["my homework ", "my phone ", "the car "];
+  let randomWhat = Math.floor(Math.random() * what.length);
+
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  excuse.innerHTML =
+    who[randomWho] + action[randomAction] + what[randomWhat] + when[randomWhen];
 };
